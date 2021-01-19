190a578fb89163803a8b78ecc87603c09a5ccb20
sau0409 <jsaurabh.0409@gmail.com>
2021-01-20 01:49:44 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector('.column__btn');
+let input = document.querySelector('.column__text');
+let colCont = document.querySelector('.column__container');
+
+
+btn.addEventListener('click', ()=> {
+    let newCol = document.createElement('div');
+    //calculate total rows possible in a row, column width: 100px + gap: 10px
+    let possibleColumnsInRow = Math.floor(window.visualViewport.width / 110);
+    newCol.innerText = input.value;
+    newCol.classList.add('col');
+    colCont.setAttribute('style', `grid-template-columns: repeat(${possibleColumnsInRow}, 100px)`)
+    colCont.appendChild(newCol);
+    input.value = ''
+});
